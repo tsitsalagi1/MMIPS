db14b42f3ddf20c0ef1f17afd14ab1c14a5ee50e
tsitsalagi1 <dynamictech.nwa@gmail.com>
2026-08-08 18:21:12 -0500

    Restore Map link in primary navigation

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,6 +37,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
               <Link href="/how-it-works">How it works</Link>
               <Link href="/profiles">Search Profiles</Link>
               <Link href="/alerts">Alerts</Link>
+              <Link href="/map">Map</Link>
               <Link href="/resources">Family Resources</Link>
               <Link href="/submit">Submit Information</Link>
             </div>
